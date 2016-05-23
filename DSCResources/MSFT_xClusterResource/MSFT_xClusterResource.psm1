b0a5d8c0b75aa3f0ba3ccbcbc9d2ebf5c8f31a5a
River Yang (ITaaS) <river.yang@itaas.dimensiondata.com>
2016-05-23 12:05:25 +1000

    1st Draft for Cluster Resource module

--- DSCResources/MSFT_xClusterResource/MSFT_xClusterResource.psm1
+++ DSCResources/MSFT_xClusterResource/MSFT_xClusterResource.psm1
@@ -1,5 +1,5 @@
 #
-# xClusterResource: DSC Resource that will Create/Remove Cluster Resource in certain Cluster Group
+# xClusterGroup: DSC Resource that will Create/Remove Cluster Group in certain Cluster
 #
 # 
 
@@ -9,16 +9,283 @@
 #
 function Get-TargetResource
 {
+    param(
+        [parameter(Mandatory)]
+        [string]$Name,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterName
+    )
+    
+    #Make sure Failover Cluster Module is imported
+    if ((Get-Module -Name FailoverClusters) -eq $null)
+    {
+        Import-Module -Name FailoverClusters
+    }
+    
+    $ClusterResource = Get-ClusterResource -Name $Name -Cluster $ClusterName
+    $ClusterResourceParam = Get-ClusterParameter -InputObject $ClusterResource
+    if ($null -ne $ClusterResource)
+    {
+        $ReturnValue = @{
+            Name = $ClusterResource.Name
+            ClusterName = $ClusterResource.Cluster.Name
+            ClusterGroupName = $ClusterResource.OwnerGroup.Name
+            ClusterResourceType = $ClusterResource.ResourceType.Name
+            Ensure = 'Present'
+            ClusterResourceParam = @(ConvertTo-CimClusterParam -InputObject $ClusterResourceParam)
+            State = $ClusterResource.State
+        }
+    }
+    else 
+    {
+        $ReturnValue = @{
+            Name = $null
+            ClusterName = $null
+            ClusterGroupName = $null
+            ClusterResourceType = $null
+            Ensure = 'Absent'
+            ClusterResourceParam = $null
+            State = $null
+        }
+    }
+    
+    $ReturnValue
 }
 #
 # The Set-TargetResource cmdlet.
 #
 function Set-TargetResource
 {
+    param(
+        [parameter(Mandatory)]
+        [string]$Name,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterName,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterGroupName,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterResourceType,
+        
+        [Microsoft.Management.Infrastructure.CimInstance[]]$ClusterResourceParam,
+        
+        [string]$State = "Online",
+        
+        [string]$Ensure = "Present"
+    )
+    
+    #Make sure Failover Cluster Module is imported
+    if ((Get-Module -Name FailoverClusters) -eq $null)
+    {
+        Import-Module -Name FailoverClusters
+    }
+    
+    $ClusterResource = Get-ClusterResource -Name $Name -Cluster $ClusterName
+    if ($Ensure -eq "Present") 
+    {
+        if ($ClusterResource -ne $null)
+        {
+            #check Parameter
+            $ClusterResourceParam_now = Get-ClusterParameter -InputObject $ClusterResource
+            foreach ($param in $ClusterResourceParam)
+            {
+                $ClusterResource_param = $ClusterResourceParam_now | Where-Object {$_.Name -eq $param.Name}
+                if ($ClusterResource_param.Value -ne $param.Value)
+                {
+                    Set-ClusterParameter -InputObject $ClusterResource -Name $param.Name -Value $param.Value
+                }
+            }
+            
+            #Check State
+            if ($ClusterResource.State -ne $State) 
+            {
+                if ($State -eq "Online") 
+                {
+                    Start-ClusterResource -InputObject $ClusterResource
+                }
+                else 
+                {
+                    Stop-ClusterResource -InputObject $ClusterResource
+                }
+            }
+            
+            if ($ClusterResource.ResourceType.Name -ne $ClusterResourceType) 
+            {
+                #Remove previous as Cluster Resource cannot have same name in different role
+                Remove-ClusterResource -Name $Name -Force
+                #Create a new Resource in the target group
+                $ClusterResourceType = $ClusterResourceType
+                $ClusterResource = Add-ClusterResource -Name $Name -Group $ClusterGroupName -ResourceType $ClusterResourceType -Cluster $ClusterName
+                foreach ($param in $ClusterResourceParam)
+                {
+                    Set-ClusterParameter -Name $param.Name -value $param.Value -InputObject $ClusterResource
+                }
+                if ($Statue -eq "Online")
+                {
+                    Start-ClusterResource -InputObject $ClusterResource
+                }
+            }
+            if ($ClusterResource.OwnerGroup.Name -ne $ClusterGroupName) 
+            {
+                #Remove previous as Cluster Resource cannot have same name in different role
+                Remove-ClusterResource -Name $Name -Force
+                #Create a new Resource in the target group
+                $ClusterResourceType = $ClusterResourceType
+                $ClusterResource = Add-ClusterResource -Name $Name -Group $ClusterGroupName -ResourceType $ClusterResourceType -Cluster $ClusterName
+                foreach ($param in $ClusterResourceParam)
+                {
+                    Set-ClusterParameter -Name $param.Name -value $param.Value -InputObject $ClusterResource
+                }
+                if ($Statue -eq "Online")
+                {
+                    Start-ClusterResource -InputObject $ClusterResource
+                }
+            }
+        }
+        else 
+        {
+            #Create a new Resource in the target group
+            $ClusterResourceType = $ClusterResourceType
+            $ClusterResource = Add-ClusterResource -Name $Name -Group $ClusterGroupName -ResourceType $ClusterResourceType -Cluster $ClusterName
+            foreach ($param in $ClusterResourceParam)
+            {
+                Set-ClusterParameter -Name $param.Name -value $param.Value -InputObject $ClusterResource
+            }
+            if ($Statue -eq "Online")
+            {
+                Start-ClusterResource -InputObject $ClusterResource
+            }
+        }
+    }
+    else
+    {
+        if ($ClusterResource -ne $null)
+        {
+            try 
+            {
+                $ClusterResource = Remove-ClusterResource -InputObject $ClusterResource -Force
+            }
+            catch [System.Exception]
+            {
+                Write-Verbose -Message  $_.Exception.Message
+            }
+        }
+    }
 }
 #
 # The Test-TargetResource cmdlet.
 #
 function Test-TargetResource
 {
+    param(
+        [parameter(Mandatory)]
+        [string]$Name,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterName,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterGroupName,
+        
+        [parameter(Mandatory)]
+        [string]$ClusterResourceType,
+        
+        [Microsoft.Management.Infrastructure.CimInstance[]]$ClusterResourceParam,
+        
+        [string]$State = "Online",
+        
+        [string]$Ensure = "Present"
+    )
+    
+    #Make sure Failover Cluster Module is imported
+    if ((Get-Module -Name FailoverClusters) -eq $null)
+    {
+        Import-Module -Name FailoverClusters
+    }
+    
+    $ClusterResource = Get-ClusterResource -Name $Name -Cluster $ClusterName
+    
+    $isDesiredState = $true
+    
+    if (($Ensure -eq "Present") -and ($ClusterResource -ne $null)) 
+    {
+        #check Parameter
+        $ClusterResourceParam_now = Get-ClusterParameter -InputObject $ClusterResource
+        foreach ($param in $ClusterResourceParam)
+        {
+            $ClusterResource_param = $ClusterResourceParam_now | Where-Object {$_.Name -eq $param.Name}
+            if ($ClusterResource_param.Value -ne $param.Value)
+            {
+                $isDesiredState = $false
+                Write-Verbose -Message "The ClusterResourceParam for Cluster Resource `"$($Name)`" does not match the desired state. "
+            }
+        }
+        
+        #Check State
+        if ($ClusterResource.State -ne $State) 
+        {
+            $isDesiredState = $false
+            Write-Verbose -Message "The State for Cluster Resource `"$($Name)`" does not match the desired state. "
+        }
+        
+        if ($ClusterResource.ResourceType.Name -ne $ClusterResourceType) 
+        {
+            $isDesiredState = $false
+            Write-Verbose -Message "The ClusterResourceType for Cluster Resource `"$($Name)`" does not match the desired state. "
+        }
+        
+        if ($ClusterResource.OwnerGroup.Name -ne $ClusterGroupName) 
+        {
+            $isDesiredState = $false
+            Write-Verbose -Message "The ClusterGroupName for Cluster Resource `"$($Name)`" does not match the desired state. "
+        }
+    }
+    
+    if (($Ensure -eq "Present" -and $ClusterResource -eq $null) -or ($Ensure -eq "Absent" -and $ClusterResource -ne $null)) 
+    {
+        $isDesiredState = $false
+        Write-Verbose -Message "The Ensure state for Cluster Group `"$($Name)`" does not match the desired state. "
+    }
+    
+    $isDesiredState
+}
+
+#functions for this Module
+# Convert Cluster Parameter to customized Class
+function ConvertTo-CimClusterParam
+{
+    <#
+    .SYNOPSIS
+        Converts Cluster Parameter elements to instances of the MSFT_xClusterParameter CIM class.
+    #>
+    [CmdletBinding()]
+    [OutputType([Microsoft.Management.Infrastructure.CimInstance])]
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [AllowEmptyCollection()]
+        [AllowNull()]
+        [Object[]]
+        $InputObject
+    )
+    begin
+    {
+        $CimClassName = 'MSFT_xClusterParameter'
+        $CimNamespace = 'root/microsoft/Windows/DesiredStateConfiguration'
+    }
+    process
+    {
+        foreach ($ClusterParam in $InputObject)
+        {
+            [Hashtable]$CimProperties = @{
+                Name = [String]$ClusterParam.Name
+                Value = [String]$ClusterParam.Value
+            }
+
+            New-CimInstance -ClassName $CimClassName -Namespace $CimNamespace -Property $CimProperties -ClientOnly
+        }
+    }
 }
